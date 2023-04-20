44bf693d254139156e70fb229bdfd62bbd85bfc1
David Brook <david.brook@lab.euc365.com>
2023-04-20 16:10:23 +0100

    Upload Disk Clean-up PR

---
+++ Windows Health/Disk Clean-up/Remediate-DiskCleanup.ps1
@@ -0,0 +1,224 @@
+<#
+.SYNOPSIS
+    A short one-line action-based description, e.g. 'Tests if a function is valid'
+.DESCRIPTION
+    A longer description of the script, its purpose, common use cases, etc.
+.NOTES
+    Information or caveats about the function e.g. 'This function is not supported in Linux'
+.LINK
+    Specify a URI to a help page, this will show when Get-Help -Online is used.
+.EXAMPLE
+    Test-MyTestFunction -Verbose
+    Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+.NOTES
+    Remember, for Proactive Remediations use:
+        - "Exit 0" - To signify in a detection script that a remediation is NOT needed and likewise in a remeditation script that it was remediated succesfully
+        - "Exit 1" - To signify in a detection script that a remediation is needed and likewise in a remeditation script that the remediation failed 
+#>
+
+#As we cannot pass Params to PR's use this first section to define the "Parameters" for the script.
+param(
+    [Parameter()]
+    [string]$LogName = 'PR-DiskCleanUp',
+    [Parameter()]
+    [string]$keyBase = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\",
+    [Parameter()]
+    [array]$keys = @(
+        "Active Setup Temp Folders",
+        "BranchCache",
+        "Downloaded Program Files",
+        "GameNewsFiles",
+        "GameStatisticsFiles",
+        "GameUpdateFiles",
+        "Internet Cache Files",
+        "Memory Dump Files",
+        "Offline Pages Files",
+        "Old ChkDsk Files",
+        "Previous Installations",
+        "Recycle Bin",
+        "Service Pack Cleanup",
+        "Setup Log Files",
+        "System error memory dump files",
+        "System error minidump files",
+        "Temporary Files",
+        "Temporary Setup Files",
+        "Temporary Sync Files",
+        "Thumbnail Cache",
+        "Update Cleanup",
+        "Upgrade Discarded Files",
+        "User file versions",
+        "Windows Defender",
+        "Windows Error Reporting Archive Files",
+        "Windows Error Reporting Queue Files",
+        "Windows Error Reporting System Archive Files",
+        "Windows Error Reporting System Queue Files",
+        "Windows ESD installation files",
+        "Windows Upgrade Log Files"
+    )
+)
+
+
+#region Functions
+function Start-Log {
+    <#
+    .SYNOPSIS
+        Creates a log file in the CCM\Logs folder or in the ProgramData\Intune\Logs folder if the device is not managed by ConfigMgr.
+    .DESCRIPTION
+        Creates a log file in the CCM\Logs folder or in the ProgramData\Intune\Logs folder if the device is not managed by ConfigMgr.
+    .PARAMETER LogName
+        The name of the log file to be created. If not specified, the name of the script will be used.
+    .PARAMETER LogFolder
+        The name of the folder to be created in the ProgramData directory to store the log file. If not specified, the default of Intune will be used.
+    .PARAMETER LogMaxSize
+        The maximum size in Mb of the log file before it is restarted. If not specified, the default of 10Mb will be used.
+    .EXAMPLE
+        Start-Log
+        Creates a log file in the CCM\Logs folder or in the ProgramData\Intune\Logs folder if the device is not managed by ConfigMgr.
+    .EXAMPLE
+        Start-Log -LogName "MyLog"
+        Creates a log file in the CCM\Logs folder or in the ProgramData\Intune\Logs folder if the device is not managed by ConfigMgr with the name MyLog.log.
+    .EXAMPLE
+        Start-Log -LogName "MyLog" -LogFolder "MyFolder"
+        Creates a log file in the CCM\Logs folder or in the ProgramData\MyFolder\Logs folder if the device is not managed by ConfigMgr with the name MyLog.log.
+    #>
+    Param (
+        [Parameter(Mandatory = $false)]
+        [string]$LogName,
+        [Parameter(Mandatory = $false)]
+        [string]$LogFolder = 'Intune',
+        [Parameter(Mandatory = $false)]
+        [string]$LogMaxSize = 10 #Mb
+    )
+
+    If (!($LogName)) {
+        $Global:LogName = $MyInvocation.MyCommand.Name
+    }
+
+    If (!($LogName)) {
+        $Global:LogName = "ScriptLog-$(Get-Date -Format FileDate)"
+    }
+
+    try {
+        If (Test-Path $env:SystemRoot\CCM) {
+            $LogPath = "$env:SystemRoot\CCM\Logs" # places the logfile in with all the other ConfigMgr logs
+        }
+        Else {
+            If (!(Test-Path $env:SystemDrive\ProgramData\$LogFolder\Logs)) {
+                New-Item $env:SystemDrive\ProgramData\$LogFolder\Logs -ItemType Directory -Force | Out-Null
+            }
+            $LogPath = "$env:SystemDrive\ProgramData\$LogFolder\Logs" # places the logfile in a Logs folder inside either the default of Intune or a folder of your choosing in the ProgramData directory
+        }
+
+        ## Set the global variable to be used as the FilePath for all subsequent Write-Log calls in this session
+        $global:ScriptLogFilePath = "$LogPath\$LogName`.log"
+        Write-Verbose "Log set to: $ScriptLogFilePath"
+
+        If (!(Test-Path $ScriptLogFilePath)) {
+            Write-Verbose "Creating new log file."
+            New-Item -Path $ScriptLogFilePath -ItemType File | Out-Null
+        }
+        else {
+            #Check if an existing log file is more than the max specified size (10Mb default) in size and restart it
+            If (((Get-Item $ScriptLogFilePath).length / 1MB) -ge $LogMaxSize) {
+                Write-Verbose "Log has reached maximum size, creating a new log file."
+                Remove-Item -Path $ScriptLogFilePath -Force | Out-Null
+                New-Item -Path $ScriptLogFilePath -ItemType File | Out-Null
+            }
+        }
+    }
+    catch {
+        Write-Error $_.Exception.Message
+    }
+}
+
+function Write-Log {
+    <#
+    .SYNOPSIS
+        Writes a message to the log file.
+    .DESCRIPTION
+        Writes a message to the log file.
+    .PARAMETER Message
+        The message to be written to the log file.
+    .PARAMETER LogLevel
+        The level of the message to be written to the log file. Valid values are 1, 2, 3, Information, Warning, Error. Default is 1.
+    .EXAMPLE
+        Write-Log -Message "This is a test message"
+        Writes a message to the log file.
+    .EXAMPLE
+        Write-Log -Message "This is a test message" -LogLevel 2
+        Writes a message to the log file with a log level of 2.
+    #>
+    Param (
+        [Parameter(Mandatory = $false)]
+        [string]$Component = $LogRegion,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Message,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet(1, 2, 3, 'Information', 'Warning', 'Error')]
+        [string]$LogLevel = 1
+    )
+
+    If (!($Component)) {
+        $Component = $MyInvocation.MyCommand.Name
+    }
+
+    If ($MyInvocation.ScriptLineNumber) {
+        $Component = "$Component - Line: $($MyInvocation.ScriptLineNumber)"
+    }
+
+    switch ($LogLevel) {
+        'Information' { [int]$LogLevel = 1 }
+        'Warning' { [int]$LogLevel = 2 }
+        'Error' { [int]$LogLevel = 3 }
+        Default { [int]$LogLevel = $LogLevel }
+    }
+
+    Write-Verbose $Message
+    $TimeGenerated = (Get-Date -Format "HH':'mm':'ss.ffffff")
+    $Thread = "$([Threading.Thread]::CurrentThread.ManagedThreadId)"
+    $Source = $MyInvocation.ScriptName
+    $Line = '<![LOG[{0}]LOG]!><time="{1}" date="{2}" component="{3}" context="" type="{4}" thread="{5}" file="{6}">'
+    $LineFormat = $Message, $TimeGenerated, (Get-Date -Format 'MM-dd-yyyy'), $Component, $LogLevel, $Thread, $Source
+    $Line = $Line -f $LineFormat
+    Try {
+        $Line | Out-File -FilePath $ScriptLogFilePath -Encoding utf8 -Append
+    }
+    catch {
+        Write-Verbose "Warning: Unable to append to log file - Retrying"
+        Try {
+            $Line | Out-File -FilePath $ScriptLogFilePath -Encoding utf8 -Append
+        }
+        catch {
+            Write-Verbose "Error: Failed to append to log file"
+        }
+    }
+}
+#endregion Functions
+
+<#
+Main script body starts here.
+#>
+
+#region Main Script
+Start-Log -LogName $LogName
+Write-Log -Message "Proactive Remediation Script Starting" -LogLevel 1 -Component "Script Start"
+try {
+    FOREACH ($key in $keys) {
+        $regPath = (Join-Path $keyBase $key)
+        IF (-not(Test-Path -Path $regPath)) {
+            Write-Log -Message "Creating registry key $regPath" -LogLevel 1 -Component "Process Registry Keys"
+            New-Item -Path $regPath -ItemType Directory -Force | Out-Null
+        }
+        Write-Log -Message "Setting StateFlags0100 to 2 for $key" -LogLevel 1 -Component "Process Registry Keys"
+        New-ItemProperty -Path (Join-Path $keyBase $key) -PropertyType DWORD -Name StateFlags0100 -Value 2 -Force | Out-Null
+    }
+    Write-Log -Message "Triggering Disk Cleanup" -LogLevel 1 -Component "Trigger Disk Cleanup"
+    Start-Process -FilePath cleanmgr.exe -ArgumentList "/sagerun:100"
+}
+catch {
+    Write-Log -Message "Error: $_.Exception.Message" -LogLevel 3 -Component "Script Error"
+}
+#endregion
